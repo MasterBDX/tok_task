55440f3c6b12477d161ddeeb06d04af430f81031
MasterBDX <masterbdxteam@gmail.com>
2021-04-02 18:23:50 +0200

    added ignore file migrate database & added Post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','timestamp']
+
+admin.site.register(Post)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    content = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images',
+                              blank=True,null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content[:20]

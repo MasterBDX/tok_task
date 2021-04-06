0ecb65c37e640e33e0418bb673f5fc603331aff8
MasterBDX <masterbdxteam@gmail.com>
2021-04-06 19:33:01 +0200

    added user field to post model

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Post(models.Model):
-    content = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images',
+    content = models.CharField(_('Content'),max_length=255)
+    image = models.ImageField(_('Content'),upload_to='images',
                               blank=True,null=True)
+    user = models.ForeignKey(User ,verbose_name=_('User'),on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
